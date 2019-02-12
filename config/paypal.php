9e202297d7ba915667b5e8d0e73592b19de42267
jaab30 <jaab30@yahoo.com>
2019-02-11 22:16:39 -0500

    fixes

--- config/paypal.php
+++ config/paypal.php
@@ -26,5 +26,5 @@ return [
     'billing_type'   => 'MerchantInitiatedBilling',
     'notify_url'     => '', // Change this accordingly for your application.
     'locale'         => '', // force gateway language  i.e. it_IT, es_ES, en_US ... (for express checkout only)
-    'validate_ssl'   => true, // Validate SSL when creating api client.
+    'validate_ssl'   => false, // Validate SSL when creating api client.
 ];
